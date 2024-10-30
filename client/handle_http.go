42a46ed08673ea9ea1c5eb10ccb733b9dc2578c0
Sarthak Agarwal <saartank9@gmail.com>
2024-10-30 22:54:02 +0000

    Corrected: Added IsDir check to prevent creation of an empty file at the destination

--- client/handle_http.go
+++ client/handle_http.go
@@ -2132,13 +2132,6 @@ func uploadObject(transfer *transferFile) (transferResult TransferResults, err e
 		return transferResult, err
 	}
 
-	if fileInfo.IsDir() {
-		log.Errorln("The given path", transfer.localPath, "is a directory. Use --recursive or -r flag if using the pelican object command for directories")
-		err := errors.New("the provided path '" + transfer.localPath + "' is a directory, but a file is expected; use --recursive or -r flag if using the pelican object command for directories")
-		transferResult.Error = err
-		return transferResult, err
-	}
-
 	var ioreader io.ReadCloser
 	nonZeroSize := true
 	pack := transfer.packOption
@@ -2160,6 +2153,12 @@ func uploadObject(transfer *transferFile) (transferResult TransferResults, err e
 		ioreader = ap
 		sizer = ap
 	} else {
+		if fileInfo.IsDir() {
+			log.Errorln("The given path", transfer.localPath, "is a directory. Use --recursive or -r flag if using the pelican object command for directories")
+			err := errors.New("the provided path '" + transfer.localPath + "' is a directory, but a file is expected; use --recursive or -r flag if using the pelican object command for directories")
+			transferResult.Error = err
+			return transferResult, err
+		}
 		// Try opening the file to send
 		file, err := os.Open(transfer.localPath)
 		if err != nil {
